f81a4e51d5f002a6b3f571bda7e3b63f2e77ebfa
Jakub <kubamajkowski1@gmail.com>
2023-10-17 18:56:40 +0200

    NavDrawer base with working DetailedActivity and back button

--- app/src/main/java/com/example/navigiationdrawer/DetailedActivity.kt
+++ app/src/main/java/com/example/navigiationdrawer/DetailedActivity.kt
@@ -2,6 +2,7 @@ package com.example.navigiationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -10,6 +11,12 @@ class DetailedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Set the parent activity for Up navigation
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
 
 //        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
@@ -28,6 +35,17 @@ class DetailedActivity : AppCompatActivity() {
             priceView.text = food.price.toString()
             imageView.setImageResource(food.image)
 
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // Respond to the Up button click
+                onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
